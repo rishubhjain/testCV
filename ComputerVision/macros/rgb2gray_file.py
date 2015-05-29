3213cb1739ce99f4bda9c4552c2671daca383b5c
rishubhjain <rishubhjain@ymail.com>
2015-05-29 02:45:11 -0700

    error handling

--- ComputerVision/macros/rgb2gray_file.py
+++ ComputerVision/macros/rgb2gray_file.py
@@ -6,7 +6,10 @@ def rgb2gray(path):
    
     
     imgread = cv2.imread (path)
-    cv2.imshow ('Origional Image',imgread)
+	if image is None:
+	    return 0
+    
+	cv2.imshow ('Origional Image',imgread)
     
     RGB2GRAY = cv2.cvtColor(imgread,cv2.COLOR_RGB2GRAY)
     #cv2.imshow ('Converted to rgb2gray',RGB2GRAY)
