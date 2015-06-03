4c790f0b35ab504c6908f1b8c34c1446bea58983
rishubh <rishubhjain47@gmail.com>
2015-06-03 11:21:11 -0700

    surf

---
+++ ComputerVision/macros/surf.py
@@ -0,0 +1,20 @@
+# SURF (Speeded-Up Robust Features)
+
+import cv2
+import numpy as np
+
+def SURF(image, hessianThreshHold):
+
+    img = cv2.imread(image)
+    hessianThreshHold1 = int(hessianThreshHold)
+    
+    surf = cv2.SURF(hessianThreshHold1)
+    
+    kp, des = surf.detectAndCompute(img,None)
+
+    img2 = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
+
+    cv2.imshow('keypoints', img2)
+    cv2.waitKey(0)
+
+    return
