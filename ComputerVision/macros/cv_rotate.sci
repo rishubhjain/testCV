970410294a86bf3e5ee02adaeba5d62bcd60563c
rishubh jain <rishubhjain@ymail.com>
2015-06-25 04:24:39 -0700

    improvements

--- ComputerVision/macros/cv_rotate.sci
+++ ComputerVision/macros/cv_rotate.sci
@@ -1,9 +1,5 @@
-
 function [image_ret] = cv_rotate(image,angle) 
 	[lhs,rhs]=argn(0)
-	pyAddToPath(pathname)
-	
-	
 	if (lhs<>1) then
 		error("this function returns an image");
 	end
@@ -11,10 +7,9 @@ function [image_ret] = cv_rotate(image,angle)
 	if(rhs<>2) then
 		error("invalid number of arguments");
 	end
-	
+	image=converttonumpy(image);
     image_ret=0
-    pyImport rotate
-    image_ret=rotate.rotate1(image,angle)
+    pyImport Rotate
+    image_ret=Rotate.rotate1(image,angle)
 
-    
 endfunction 
--- ComputerVision/src/python/rotate.py
+++ ComputerVision/src/python/rotate.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
-
+import cvcheck_channel
 def rotate1(image,angle):
-    #refine this function for angle in float value
-    rows,cols = img.shape   
     if image is None:
             print 'Error in input Image'
 	    return 0
+    if cvcheck_channel.check_channel(image)==2:
+        rows,cols = image.shape
+    elif cvcheck_channel.check_channel(image)==3:
+        rows,cols,depth = image.shape 
+    
     angle=int(angle)
 
     M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
