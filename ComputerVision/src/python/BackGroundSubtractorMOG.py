da2df07f0962c346ffe90a0c0376603adb1418e8
rishubh jain <rishubhjain@ymail.com>
2015-07-18 09:05:37 -0700

    background subtraction

---
+++ ComputerVision/src/python/BackGroundSubtractorMOG.py
@@ -0,0 +1,22 @@
+#BackgroundSubtractorMOG
+#As an argument pass video
+
+import cv2
+import numpy as np
+
+def BackgroundSubtractorMOG(video):
+
+    capture = cv2.VideoCapture(video)
+
+    fgbg = cv2.createBackGroundSubtracrorMOG()
+
+    while(1):
+        ret, frame = capture.read()
+        fgmask = fgbg.apply(frame)
+        cv2.imshow('frame',fgmask)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+    capture.release()
+    cv2.destroyAllWindows()
+    return
