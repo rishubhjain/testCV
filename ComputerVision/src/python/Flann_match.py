fc97d28f7d1934f550d7ca47ef05abbb157fb299
rishubh jain <rishubhjain@ymail.com>
2015-07-20 11:24:06 -0700

    flann

---
+++ ComputerVision/src/python/Flann_match.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+
+def flann_match(des1,des2,num_trees,num_checks,k):
+    num_trees=int(num_trees)
+    num_checks=int(num_checks)
+    FLANN_INDEX_KDTREE = 0
+    k=int(k)
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = num_trees)
+    search_params = dict(checks = num_checks)
+    
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1,des2,k)
+
+    return matches
