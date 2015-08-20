5e20d11bc693c89b0a7ecb1e8cb57c8d2022ea64
rishubh jain <rishubhjain@ymail.com>
2015-08-20 10:59:58 +0530

    cv_ellipse changes

--- ComputerVision/src/python/Ellipse.py
+++ ComputerVision/src/python/Ellipse.py
@@ -2,24 +2,23 @@ import cv2
 import numpy as np
 import Point
 
-#angle can be float
-def ellipse(img,center, axes, angle, startAngle, endAngle,color,thickness,lineType):
-	center=Point.point_int(center)
-    color=Point.point_int(color)	
-    axes=Point.point_int(axes)
-	angle=int(angle)
-	startAngle=int(startAngle)
-	endAngle=int(endAngle)
-	thickness=int(thickness)
-	
-	  if linetype == 8 :
+
+def ellipse(img,center, axes, angle, startAngle, endAngle,color,thickness,linetype):
         
-		cv2.ellipse(img, center,axes,angle, startAngle, endAngle,color,thickness,lineType=CV_AA)
-    elif linetype == 4 :
-        
-		cv2.ellipse(img, center,axes,angle, startAngle, endAngle,color,thickness,lineType=CV_AA)
-    else:
-        
-		cv2.ellipse(img, center,axes,angle, startAngle, endAngle,color,thickness,lineType=CV_AA)
-    
-    return img
+        center=Point.point_int(center)
+        color=Point.point_int(color)	
+        axes=Point.point_int(axes)
+        angle=int(angle)
+        startAngle=int(startAngle)
+        endAngle=int(endAngle)
+        thickness=int(thickness)
+
+        if linetype == 8 :
+
+                cv2.ellipse(img, center,axes,angle, startAngle, endAngle,color,thickness,lineType=8)
+        elif linetype == 4 :	
+
+                cv2.ellipse(img, center,axes,angle, startAngle, endAngle,color,thickness,lineType=4)
+       
+
+        return img
